7b8556c2e52f9242949bb6cb7b2f5b2915c59df8
Nick Niebaum <nick@nickniebaum.com>
2014-11-11 15:22:46 -0500

    Created deploy.sh to setup file permissions

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-lessc "$DIR/../assets/less/application.less" "$DIR/../assets/css/application.css"
-lessc "$DIR/../assets/less/admin.less" "$DIR/../assets/css/admin.css"
-lessc "$DIR/../assets/less/index.less" "$DIR/../assets/css/index.css"
-lessc "$DIR/../assets/less/index_mobile.less" "$DIR/../assets/css/index_mobile.css"
+
+chmod 777 ../assets/css/application.css
+chmod 777 ../assets/css/application.css.map
+chmod 777 ../assets/scss/includes/_theme.scss
+chmod 777 ../assets/img/layout/homepage-intro.png
+chmod -R 777 ../assets/uploads
